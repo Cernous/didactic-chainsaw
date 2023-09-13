75f3859699e7bb489df9587c2e3038f2994b1ea4
Cernous <redtraitor9@gmail.com>
2023-09-13 17:07:18 -0400

    feat: Workshop3

--- Archive/src/main/java/Tutorial1/main.java
+++ Archive/src/main/java/Tutorial1/main.java
@@ -1,5 +1,5 @@
 package Tutorial1;
-
+/*
 public class main {
     public static void main() {
         int results_i = Utility.addIntNumbers(3, 4);
@@ -7,4 +7,4 @@ public class main {
         System.console().printf("%s%n", "Int Results: " + results_i + "\n");
         System.console().printf("%s%d", "Double Results: " + results_f + "\n");
     }
-}
+}*/
